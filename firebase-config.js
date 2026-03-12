1673f76019f0e2c5dbddc1e7da5dde40d048d68c
aqsagaza20 <aqsa.gaza20@gmail.com>
2026-03-12 04:32:08 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,22 +3,21 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
-// إعدادات Firebase الخاصة بمشروع NADER
-// تم نسخها من Firebase Console
+// إعدادات Firebase الخاصة بك (نسختها من موقع Firebase)
 const firebaseConfig = {
-    apiKey: "AIzaSyOj7tZ67zeIARUo0P4z___________________", // أكمل القيمة من الموقع
+    apiKey: "AIzaSyD70tZ67zeIARlOzOfP4zALbPjfmef31E8",
     authDomain: "nader-c1691.firebaseapp.com",
-    databaseURL: "https://nader-c1691.firebaseio.com", // لاحظ التعديل هنا
     projectId: "nader-c1691",
-    storageBucket: "nader-c1691.appspot.com", // لاحظ التعديل هنا (حذفت .firebase)
-    messagingSenderId: "455346952959",
-    appId: "1:456346892595:web:8e4afdc7dcd22d509690db8a"
+    storageBucket: "nader-c1691.firebasestorage.app",
+    messagingSenderId: "455340592959",
+    appId: "1:455340592959:web:8e4adc7dcd22d509690d8a",
+    measurementId: "G-0WB2GLZGCL"
 };
 
 // تهيئة Firebase
 const app = initializeApp(firebaseConfig);
 
-// تصدير الخدمات التي سنستخدمها
+// تهيئة الخدمات التي سنستخدمها
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
